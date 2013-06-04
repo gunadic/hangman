56336aecac9c0a68a5a58f4adc15511d50b34921
Chris Gunadi <gunadic@gmail.com>
2013-06-04 16:26:26 -0400

    adds all models, spec files and a runner

---
+++ lib/main.rb
@@ -0,0 +1,37 @@
+def check_num_players (input)
+  (1..5).member?(input.to_i)
+end
+
+def get_secondary_input
+  puts "Please enter a number between 1 and 5"
+  player_count = gets.chomp
+  if check_num_players(player_count)==false
+    get_secondary_input
+  else
+    return player_count
+  end
+end
+
+puts "How many players do you want?"
+
+player_count = gets.chomp
+
+if check_num_players(player_count) == false
+  puts "Please enter a number between 1 and 5."
+  player_count = gets.chomp
+end
+
+
+setup = GameSetup.new(player_count)
+
+setup.num_players.times do |i|
+  result = false
+  while !result
+    puts "What is player #{i}'s name?"
+    result = setup.add_player(gets.chomp)
+    if !result
+      puts "Name has already been taken"      
+    end
+  end
+end
+
