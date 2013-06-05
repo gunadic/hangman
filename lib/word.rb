496757fad374b6dc72ab737723ad4f19aaf5fe7d
Chris Gunadi <gunadic@gmail.com>
2013-06-04 20:05:54 -0400

    Removed the possibility of random phrases and nouns, because random-word gem includes scientific names which means spaces...

--- lib/word.rb
+++ lib/word.rb
@@ -4,17 +4,7 @@ require 'random-word'
 class Word
   attr_accessor :the_word, :display_array
   def initialize
-    # temp = rand(3)
-    # if temp == 0
-    #   @the_word = RandomWord.adjs.next
-    # elsif temp == 1 
-    #   @the_word = RandomWord.nouns.next
-    # elsif temp == 2
-    #   @the_word = RandomWord.phrases.next
-    # end
-
-    @the_word = "monkey"
-    # For TESTING purposes ONLY
+    @the_word = RandomWord.adjs.next
     @display_array = []
     @the_word.length.times do @display_array << "_" end
     @filled = false
